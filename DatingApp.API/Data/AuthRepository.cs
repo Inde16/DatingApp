f79ba1ef15bd39664edb2f392e2fbfef2d2b9794
Inde16 <sotinde@gmail.com>
2018-09-21 16:57:33 +0300

    Second commit

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Modules;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Name == username);
+
+            if (user == null)
+                return null;
+            
+            if(!VerifyPasswordHash(password , user.PasswordHash , user.PasswordSalt))
+                return null;
+
+        return null;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hwin = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                
+               var computedHash = hwin.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+               for(int i = 0; i < computedHash.Length; i++)
+               {
+                   if(computedHash != passwordHash)
+                   return false;
+               }
+            
+            }
+            return true;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash , passwordSalt;
+            CreatePasswordHas(password , out passwordHash ,out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context .Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHas(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hwin = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hwin.Key;
+                passwordHash = hwin.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+
+        public async Task<bool> UserExist(string username)
+        {
+            if (await _context.Users.AnyAsync(x => x.Name == username))
+            return true;
+
+        return false;
+        }
+    }
+}
