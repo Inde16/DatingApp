63c390038087eeb9a2edca8642b0bc33987bd1eb
Inde16 <sotinde@gmail.com>
2018-10-15 15:27:36 +0300

    Commit v.18

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -8,5 +8,19 @@ namespace DatingApp.API.Dtos
         [Required]
         [StringLength(12 , MinimumLength = 4 , ErrorMessage = "You must specify password between 4 and 12 characters")]
         public string Password{ get; set; }
+        public string Gender { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+
+        }
     }
 }
